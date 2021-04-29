ab504d03f83333d90cccf8b567a16a5beb77466b
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-04-29 12:25:09 +0530

    Create Infinite_for_loop.java

---
+++ Infinite_for_loop.java
@@ -0,0 +1,10 @@
+public class Forloop
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		for(int i=0;i<100;i-=5)
+		{
+		    
+		    System.out.println(i);
+		    
+		}
