3551b40de84f3d37cc9a5de1f9775d22b8822828
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-04-29 07:53:33 +0530

    Create Basic_for_loop_programme.java

---
+++ Basic_for_loop_programme.java
@@ -0,0 +1,12 @@
+public class Forloops
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+		String name ="ASDFGHJ";
+		for(int i=0;i<=name.length()-1;i++)
+		{
+		    System.out.println("char:"+i);
+		}
+	}
+}
