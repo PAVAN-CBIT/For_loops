4e6d27a0ef2b1169e76eeb1e4f5b5ff2eea9f3b5
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-04-29 13:51:26 +0530

    Create For_loop.java

---
+++ For_loop.java
@@ -0,0 +1,18 @@
+public class forloop
+{
+	public static void main(String[] args) 
+   {
+		System.out.println("Hello World");
+		int counter=0;
+		for(int i=0;i<=100;i++)
+		
+		   {
+		   
+		      System.out.println(i);
+	
+		   }
+		
+		
+	 }
+  
+}
