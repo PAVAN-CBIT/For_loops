70fdee14af09b4eb72e8432340cfcbb10c0b2880
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-04-29 12:15:19 +0530

    Create Reverse_Number.java

---
+++ Reverse_Number.java
@@ -0,0 +1,14 @@
+class ReversePrint
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World!");
+        String Str ="asdfghj";
+        for(int i=Str.length()-1;i>=0;i--)
+        {
+            
+            System.out.println(Str.charAt(i));
+            
+        }
+    }
+}
