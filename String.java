5ef23aeb77975b1b5b919d0d2185186f6b9eebed
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-05-01 11:20:08 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,24 @@
+Public class Forloop
+{
+
+  Public Static void main(String args[])
+  {
+  
+  
+    for(int i=0;i<n;i++)
+    
+    {
+        System.out.println(i);  
+    }
+  
+  
+  }
+  
+}
+
+
+
+
+
+
+}
